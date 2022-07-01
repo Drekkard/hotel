5a3a94ea9f0ce5c205789c8ba41f7fb750f83558
start.vaadin.com <no-reply@vaadin.com>
2022-07-01 07:22:53 +0000

    Generated project

---
+++ src/main/java/hotel/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package hotel.data.service;
+
+import hotel.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
